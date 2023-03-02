31affbaf3f6d6a0ccaad78fd87612305a1a8ec1c
MuthamakiCreations <muthamakicreations@gmail.com>
2023-03-02 11:54:15 -0800

    Concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,22 @@
-#include <main.h>
-#include <string.h>
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
 
-int main(){
-	char string1[]="Hi guys, ";
-	char string2[]="welcome to my channel.";
-	strcat(string1, string2);
-	printf("%s", string1);
-	return 0;
+char *_strcat(char *dest, char *src)
+{
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
