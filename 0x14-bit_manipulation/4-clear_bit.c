b88d4915e5cde684ad702827fd212a14fcfa79ee
MuthamakiCreations <muthamakicreations@gmail.com>
2023-04-06 07:51:04 -0700

    Task5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * zero_bit - sets the value of a bit to 0 at a given index
+ * clear_bit - set the value of a bit to 0 at a given index
  * @n: pointer to decimal number to change
  * @index: index position to change
  * Return: 1 if it worked, -1 if error
  */
 
-int zero_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
 	unsigned int hold;
