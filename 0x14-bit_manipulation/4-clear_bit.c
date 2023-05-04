e1da475b28c2a5df3983d0847a2292cee5d81711
MuthamakiCreations <muthamakicreations@gmail.com>
2023-05-04 14:09:36 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - bit value set to 0 at a given index
+ * @n: the pointer of the  decimal no changes
+ * @index: index changes
+ * Return: 1 if success, -1 if there's an error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x;
+	unsigned int y;
+
+	if (index > 64)
+		return (-1);
+	hold = index;
+i	for (x = 1; y > 0; i *= 2, y--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= x;
+
+	return (1);
+}
